a26fa3cee9c4b525cc958f0eec0388e56d95384a
Rachel <rachel@rachelrockaway.com>
2015-06-01 01:44:06 -0400

    Initial Commit

---
+++ Stoke Patrol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stoke Patrol
+//
+//  Created by Rachel Stevenson on 6/1/15.
+//  Copyright (c) 2015 rachelrockaway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
