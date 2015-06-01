19f3946a3e5c3010adf6ae1ce4b8f05b22699170
Rachel <rachel@rachelrockaway.com>
2015-06-01 01:54:04 -0400

    Initial commit. Add frameworks to support Parse, menu icon images, and bridging header file.

---
+++ bridging header.m
@@ -0,0 +1,9 @@
+//
+//  bridging header.m
+//  Stoke Patrol
+//
+//  Created by Rachel Stevenson on 6/1/15.
+//  Copyright (c) 2015 rachelrockaway. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
