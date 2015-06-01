7375744a157312d8327ab39ca98556262759f704
Rachel <rachel@rachelrockaway.com>
2015-06-01 02:05:40 -0400

    Run Parse test.

--- Stoke Patrol/ViewController.swift
+++ Stoke Patrol/ViewController.swift
@@ -6,12 +6,14 @@
 //  Copyright (c) 2015 rachelrockaway. All rights reserved.
 //
 
+import Parse
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
